0f0968294b82052f0bff33b6ccce157888f98b5c
Juan Antonio Valera <j.v.metall@gmail.com>
2015-04-27 13:31:35 +0200

    Creado formulario de login
    Aun no hay usuarios y no funciona

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -8,10 +8,26 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/", name="portada")
      */
     public function indexAction()
     {
         return $this->render('default/index.html.twig');
     }
+
+    /**
+     * @Route("/entrar", name="usuario_entrar",methods={"GET"})
+     */
+    public function entrarAction()
+    {
+        $helper = $this->get('security.authentication_utils');
+        return $this->render('AppBundle:App:entrada.html.twig',
+            [
+                'last_username' => $helper->getLastUsername(),
+                'error'         => $helper->getLastAuthenticationError()
+            ]);
+    }
 }
+
+
+
